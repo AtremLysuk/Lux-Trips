8ae4dd1bdd14c09bd868cfac3f75cbd5d71a0b08
Artem Lysuk <artemlysuk36@gmail.com>
2026-01-21 17:45:30 +0200

    fix head title

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -45,6 +45,7 @@ const Header = () => {
   ];
 
 
+
   useGSAP(
     () => {
       if (!isBurgerOpen) {
--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,32 +1,33 @@
-import { Link, useNavigate } from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import '@/scss/blocks/_notfound-page.scss';
-
-
-
 
 const NotFoundPage = () => {
   const navigate = useNavigate()
   console.log(navigate)
-
-
   const goBack = () => {
     navigate(-1)
   }
-
-  return (
-    <section className="notfound" aria-labelledby='notFound-title' >
+  return (<section
+      className="notfound"
+      aria-labelledby='notFound-title'
+    >
       <div className="notfound__container container">
         <div className="notfound__inner">
-          <h2 className="notfound__title h2" id='notFound-title'>Page Not Found</h2>
+          <h2
+            className="notfound__title h2"
+            id='notFound-title'
+          >Page Not Found
+          </h2>
           <div className="notfound__inner__link">
-            <Link  to='/' onClick={goBack} >
+            <Link
+              to='/'
+              onClick={goBack}
+            >
               Go back
             </Link>
           </div>
         </div>
       </div>
-    </>
-  );
+    </section>);
 };
-
 export default NotFoundPage;
